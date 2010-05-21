8d13d4019cce8eb487a24dd3795c3e289d396668
Edwin Shin <edwin.shin@yourmediashelf.com>
2010-05-21 10:31:59 +0200

    added Maven release plugin

--- src/main/java/com/yourmediashelf/fedora/client/FedoraClient.java
+++ src/main/java/com/yourmediashelf/fedora/client/FedoraClient.java
@@ -13,7 +13,6 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
-import com.sun.jersey.api.client.filter.LoggingFilter;
 import com.sun.jersey.client.urlconnection.HTTPSProperties;
 import com.yourmediashelf.fedora.client.request.FedoraRequest;
 
@@ -79,7 +78,7 @@ public class FedoraClient {
         wr
                 .addFilter(new HTTPBasicAuthFilter(fc.getUsername(), fc
                         .getPassword()));
-        wr.addFilter(new LoggingFilter(System.out));
+        //wr.addFilter(new LoggingFilter(System.out));
         return wr;
     }
 
