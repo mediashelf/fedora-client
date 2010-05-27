4623309d7486bfdbfa0352b2dce75e6fb0adedda
Edwin Shin <edwin.shin@yourmediashelf.com>
2010-05-27 20:33:14 +0200

    support adding any kind of relationship

--- src/main/java/com/yourmediashelf/fedora/client/RelsExt.java
+++ src/main/java/com/yourmediashelf/fedora/client/RelsExt.java
@@ -1,7 +1,13 @@
 package com.yourmediashelf.fedora.client;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 /**
  * A builder for a Fedora 3.x RELS-EXT datastream.
@@ -14,43 +20,68 @@ import java.util.Set;
 public class RelsExt {
 	public static class Builder {
 		private final String pid;
-		private final Set<String> cmodels = new HashSet<String>();
+		private final Resource subject;
+		private final Model model = ModelFactory.createDefaultModel();
 
 		public Builder(String pid) {
 			if (pid.startsWith("info:fedora/")) {
 				pid = pid.substring(12);
 			}
 			this.pid = pid;
-			cmodels.add("info:fedora/fedora-system:ContentModel-3.0");
+			model.setNsPrefix("fedora-model", "info:fedora/fedora-system:def/model#");
+			this.subject = model.createResource(String.format("info:fedora/%s", pid));
+			addCModel("info:fedora/fedora-system:ContentModel-3.0");
 		}
 
 		public RelsExt build() {
-			RelsExt result = new RelsExt(pid);
-			result.cmodels = cmodels;
+			RelsExt result = new RelsExt(pid, model);
 			return result;
 		}
 
-		public Builder cmodel(String cmodel) {
-			if (cmodel == null) {
-				return this;
-			}
+		public Builder addRelationship(String predicate, String object, boolean isLiteral, String datatype) {
+		    Property p = model.createProperty(predicate);
+		    RDFNode o;
+		    if (isLiteral) {
+		        o = model.createTypedLiteral(object, datatype);
+		    } else {
+		        o = model.createResource(object);
+		    }
+		    model.add(subject, p, o);
+		    return this;
+		}
+
+		public Builder addCModel(String cmodel) {
+			Property p = model.createProperty("info:fedora/fedora-system:def/model#hasModel");
 			if (!cmodel.startsWith("info:fedora/")) {
 				cmodel = "info:fedora/" + cmodel;
 			}
-			cmodels.add(cmodel);
+			RDFNode o = model.createResource(cmodel);
+			model.add(subject, p, o);
 			return this;
 		}
 	}
 
 	private final String pid;
-	private Set<String> cmodels = new HashSet<String>();
+	private final Model model;
 
-	private RelsExt(String pid) {
+	private RelsExt(String pid, Model model) {
 		this.pid = pid;
+		this.model = model;
 	}
 
 	@Override
     public String toString() {
+	    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+	    model.write(bout);
+	    try {
+            return bout.toString("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+
+	    /*
 		String cmodelStatements = "";
 		for (String cmodel : cmodels) {
 			cmodelStatements += String
@@ -63,13 +94,10 @@ public class RelsExt {
 			   "%s" +
 			   "  </rdf:Description>" +
 			   "</rdf:RDF>", pid, cmodelStatements);
+	   */
 	}
 
 	public String getPid() {
 		return pid;
-	}
-
-	public Set<String> getCmodels() {
-		return cmodels;
 	}
 }
