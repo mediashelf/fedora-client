1f757ba219b7a9480d153b0dfd7f867fdc840da2
Edwin Shin <edwin.shin@yourmediashelf.com>
2010-05-29 12:22:20 +0200

    new test case for findobjects

---
+++ src/test/java/com/yourmediashelf/fedora/client/request/FindObjectsTest.java
@@ -0,0 +1,20 @@
+
+package com.yourmediashelf.fedora.client.request;
+
+import static com.yourmediashelf.fedora.client.FedoraClient.findObjects;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.yourmediashelf.fedora.client.response.FedoraResponse;
+
+public class FindObjectsTest extends FedoraMethodBaseTest {
+
+    @Test
+    public void testFindObjects() throws Exception {
+        FedoraResponse response = null;
+
+        response = findObjects().pid(true).terms("*").resultFormat("xml").execute(fedora());
+        assertEquals(200, response.getStatus());
+    }
+}
