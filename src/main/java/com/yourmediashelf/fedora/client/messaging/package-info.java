6db6f88e21b2ce63dcb448e4ca57a8c4bf99595e
Edwin Shin <edwin.shin@yourmediashelf.com>
2010-07-06 12:10:39 +0200

    license headers

--- src/main/java/com/yourmediashelf/fedora/client/messaging/package-info.java
+++ src/main/java/com/yourmediashelf/fedora/client/messaging/package-info.java
@@ -1,4 +1,23 @@
 /**
+ * Copyright (C) 2010 MediaShelf <http://www.yourmediashelf.com/>
+ *
+ * This file is part of fedora-client.
+ *
+ * fedora-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * fedora-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with fedora-client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
  * <p>This package is essentially the same as org.fcrepo.client.messaging but
  * without all of the org.fcrepo.server and org.fcrepo.common dependencies
  * (as none of the org.fcrepo artifacts are actually available in a Maven
